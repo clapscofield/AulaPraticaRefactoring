6f4a68ac5cc6f8426c3250272c6a1292498116c1
clapscofield <clarisse.scofield@gmail.com>
2021-03-06 12:26:36 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -1,12 +1,9 @@
 public abstract class Price {
-    public abstract int getPriceCode();
-    
-    public abstract double getCharge(int daysRented);
+  public abstract int getPriceCode();
 
-    public int getFrequentRenterPoints(int daysRented) {
-    if ((getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) return 2;
+  public abstract double getCharge(int daysRented);
+
+  public int getFrequentRenterPoints(int daysRented) {
     return 1;
   }
-
 }
