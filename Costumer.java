7bbbd523b9b291fda8b7e9f2dbc3b1577b176a45
clapscofield <clarisse.scofield@gmail.com>
2021-03-06 12:15:03 -0300

    Commit 7

--- Costumer.java
+++ Costumer.java
@@ -38,9 +38,27 @@ public class Customer {
 
      }
      //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
+     result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+     result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
+    
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        result += each.getFrequentRenterPoints();
+    }
      return result;
    }
 } 
